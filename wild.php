e2ddb77dad1c5fa76eb38b1a658801480a8aa839
ni33wild <nicola.trentini.wild@gmail.com>
2018-10-09 11:49:55 +0200

    A la Wild Code school on code en chaussettes.

---
+++ wild.php
@@ -0,0 +1,7 @@
+<?php
+$nameOfFilm = "Indiana Jones and the Last Crusade";
+$isSeen = true; 
+$year = 1989;
+$voteImdb = 8.3;
+echo "Film name VO : ".$nameOfFilm."\r\n"."isSeen : ". $isSeen."\r\n"."Release : ". $year. "\r\n"."Rating : ". $voteImdb. "\r\n";
+?>
